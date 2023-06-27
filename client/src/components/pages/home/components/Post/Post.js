03dc0797855d249eb43741b9cd7554370db6dca9
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-06-28 01:27:32 +0400

    add Post component

---
+++ client/src/components/pages/home/components/Post/Post.js
@@ -0,0 +1,32 @@
+import AnyText from "../../../../usable-components/more/AnyText"
+import ProfileTitle from "../../../../usable-components/profile/ProfileTitle/ProfileTitle"
+import PostImage from "../PostImage/PostImage"
+import PostTools from "../PostTools/PostTools"
+
+function Post(){
+    return(
+        <>
+            <div class="post-top-part">
+                <ProfileTitle userName={"Ruben"}/>
+                {/* <div class="post-manu">
+                    <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+                </div> */}
+            </div>
+            <PostImage/>
+            <PostTools/>
+            <AnyText 
+                divClass={"post-likes-comments"} 
+                pClass={"post-likes"}
+                text={"1,238 likes"}
+            />
+            <AnyText 
+                divClass={"post-description"} 
+                pClass={"post-description-text"}
+                text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quia impedit dolore sunt facilis temporibus tempore numquam laudantium. Unde, tempore."}
+            />
+            <hr/>
+        </>
+    )
+}
+
+export default Post
