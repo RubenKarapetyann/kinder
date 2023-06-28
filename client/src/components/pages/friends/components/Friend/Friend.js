3ef602587485488bdf5f1094c0dd2837fc6119f6
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-06-28 15:05:37 +0400

    profileTitle with children

--- client/src/components/pages/friends/components/Friend/Friend.js
+++ client/src/components/pages/friends/components/Friend/Friend.js
@@ -4,18 +4,17 @@ import { FRIENDS_DROPDOWN } from "../../../../../constants/friends-constants"
 
 const Friend = ()=>{
     return(
-        <ProfileTitle 
-                userName={"Ruben"}
-                func={<div class="friend-last-message-age">
-                <div class="dropdown text-end">
-                    <a href="#" class="d-block link-body-emphasis text-decoration-none" data-bs-toggle="dropdown" aria-expanded="false"> 
-                        <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+        <ProfileTitle userName={"Ruben"}>
+            <div className="friend-last-message-age">
+                <div className="dropdown text-end">
+                    <a href="#" className="d-block link-body-emphasis text-decoration-none" data-bs-toggle="dropdown" aria-expanded="false"> 
+                        <i className="fa fa-ellipsis-h" aria-hidden="true"></i>
                         ...
                     </a>
                     <DropDownItemList list={FRIENDS_DROPDOWN}/>
                 </div>
-            </div>}
-        />
+            </div>
+        </ProfileTitle>
     )
 }
 
--- client/src/components/usable-components/profile/ProfileTitle/ProfileTitle.js
+++ client/src/components/usable-components/profile/ProfileTitle/ProfileTitle.js
@@ -1,6 +1,6 @@
 import "./ProfileTitle.css"
 
-const ProfileTitle = ({ userName, comment, func })=>{
+const ProfileTitle = ({ userName, comment, children })=>{
     return(
         <div class="profile-title-container">
             <img src="https://ionicframework.com/docs/img/demos/avatar.svg" alt="mdo" width="32" height="32" class="rounded-circle"/>
@@ -9,7 +9,7 @@ const ProfileTitle = ({ userName, comment, func })=>{
                     <span class="profile-title-username">{userName}</span>
                     <span class="profile-title-comment">{comment}</span>
                 </div>
-                <div class="profile-title-function">{func}</div>
+                <div class="profile-title-function">{children}</div>
             </div>
         </div>
     )
