2c7f0dfddd826eb77f432d0b7b3ceebb6aaba86a
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-06-29 13:00:32 +0400

    register data structure

--- client/src/components/pages/register/Register.js
+++ client/src/components/pages/register/Register.js
@@ -1,9 +1,16 @@
+import { useState } from "react"
 import FormError from "../../usable-components/form/FormError"
 import FormInput from "../../usable-components/form/FormInput"
 import FormText from "../../usable-components/form/FormText"
 import FormTitle from "../../usable-components/form/FormTitle"
 
 function Register(){
+    const [inputData,setInputData] = useState({
+        name : "",
+        email : "",
+        password : "",
+        repeatPassword : ""
+    })
     return(
         <div className="form-container">
             <FormTitle text={"Register"}/>
@@ -13,21 +20,21 @@ function Register(){
                     type={"text"} 
                     name={"Name"}
                     // changeHandle={emailChange}
-                    // value={inputData.email}
+                    value={inputData.name}
                 />
 
                 <FormInput 
                     type={"email"} 
                     name={"Email"}
                     // changeHandle={emailChange}
-                    // value={inputData.email}
+                    value={inputData.email}
                 />
         
                 <FormInput 
                     type={"password"} 
                     name={"Password"}
                     // changeHandle={passwordChange}
-                    // value={inputData.password}
+                    value={inputData.password}
                 />
 
 
@@ -35,7 +42,7 @@ function Register(){
                     type={"password"} 
                     name={"Repeat Password"}
                     // changeHandle={passwordChange}
-                    // value={inputData.password}
+                    value={inputData.repeatPassword}
                 />
                 <FormError />
     
