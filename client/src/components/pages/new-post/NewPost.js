ceda64fb22075824bc6c1107b8d30047596777cd
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-06-30 01:20:24 +0400

    new post component description input

--- client/src/components/pages/new-post/NewPost.js
+++ client/src/components/pages/new-post/NewPost.js
@@ -1,6 +1,22 @@
+import { useRef, useState } from "react"
+import FormInput from "../../usable-components/form/FormInput"
+import "./css/NewPost.css"
+
 function NewPost(){
+    const [value,setValue] = useState("")
+    const changeHandle = e => setValue(e.target.value)
+
+
+    let fileLoaderRef = useRef(null)
     return(
-        <p>post</p>
+        <div className="posting-container">
+            <div className="mb-3">
+                <label for="formFile" className="form-label"></label>
+                <input className="form-control" type="file" id="formFile" ref={fileLoaderRef}/>
+            </div>
+            <FormInput name={"Description"} type={"text"} value={value} changeHandle={changeHandle}/>
+            <button className="btn btn-primary">Post</button>
+        </div>
     )   
 }
 
