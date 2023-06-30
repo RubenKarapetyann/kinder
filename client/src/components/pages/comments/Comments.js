897e106d1158ee21ece520d120eb68bfe8bf5d6c
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-06-30 17:53:24 +0400

    comments page

--- client/src/components/pages/comments/Comments.js
+++ client/src/components/pages/comments/Comments.js
@@ -1,7 +1,22 @@
+import Message from "../../usable-components/messages/message/Message"
+import MessageInput from "../../usable-components/messages/messageInput/MessageInput"
+import { FaArrowLeft } from "react-icons/fa"
+import "./css/Comments.css"
 
 function Comments(){
     return(
-        <>comments</>
+        <>
+            <div className="back-arrow">
+                <FaArrowLeft/>
+            </div>
+            <div style={{
+                minHeight: "70vh"
+            }}>
+                <Message text={"hello"}/>
+                <Message text={"darov"}/>
+            </div>
+            <MessageInput/>
+        </>
     )   
 }
 
