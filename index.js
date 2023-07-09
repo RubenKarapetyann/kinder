0ccfe4ab15f6f39c8026f2b248b670b8b12103fd
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-09 18:32:10 +0400

    delete friend by id from database

--- index.js
+++ index.js
@@ -405,7 +405,19 @@ app.get(FRIENDS,passport.authenticate("jwt", {session : false}),(req,res)=>{
 
 app.delete(FRIENDS+"/:id",passport.authenticate("jwt", {session : false}),(req,res)=>{
     const { id } = req.params
+    const { id:userId } = req.user
+    const users = JSON.parse(fs.readFileSync('./database/users.json',{ encoding: 'utf8', flag: 'r' }))
+    const chat = JSON.parse(fs.readFileSync('./database/messages.json',{ encoding: 'utf8', flag: 'r' }))
+    
+    let chatId 
+    users[userId].friends = users[userId].friends.filter(friend=>friend.friendId===id ? (chatId=friend.chatId) && false : true)
+    users[id].friends = users[id].friends.filter(friend=>friend.friendId!==userId)
+    delete chat[chatId]
 
+
+
+    fs.writeFileSync("./database/users.json", JSON.stringify(users,undefined,2));
+    fs.writeFileSync("./database/messages.json", JSON.stringify(chat,undefined,2));
 
     res.send({
         access : true
