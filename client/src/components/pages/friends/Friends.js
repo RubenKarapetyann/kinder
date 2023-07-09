c2980322a9abfbb144f414d24400be969462724d
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-09 13:39:52 +0400

    select friends and loading from state

--- client/src/components/pages/friends/Friends.js
+++ client/src/components/pages/friends/Friends.js
@@ -1,13 +1,23 @@
 import { FRIENDS_DROPDOWN } from "../../../constants/friends-constants"
 import DropDown from "../../usable-components/dropdown/DropDown"
 import ProfileListItem from "../../usable-components/profile/ProfileListItem"
+import { useSelector } from "react-redux"
 
 function Friends(){
+    const { loading, list } = useSelector(store=>store.friends)
+
+
+    if(loading){return<p>loading...</p>}
     return (
         <>
-            <ProfileListItem userName={"Ruben"}>
-                <DropDown list={FRIENDS_DROPDOWN}/>
-            </ProfileListItem>
+            {list.map(friend=>{
+                return (
+                    <ProfileListItem userName={"Ruben"}>
+                        <DropDown list={FRIENDS_DROPDOWN}/>
+                    </ProfileListItem>
+                )
+            })}
+            
         </>
     )
 }
