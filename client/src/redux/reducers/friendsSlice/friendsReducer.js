72957dbd10c39dbf68e68a32ed49afd6a4ba0733
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-09 15:54:50 +0400

    friends slice loading

--- client/src/redux/reducers/friendsSlice/friendsReducer.js
+++ client/src/redux/reducers/friendsSlice/friendsReducer.js
@@ -1,7 +1,18 @@
+import { LOADING_START, LOADING_FINISH } from "../../../constants/friends-slice-constants"
 
 
 function friendsReducer(state={ loading : false, list : [] },action){
     switch(action.type){
+        case LOADING_START:
+            return {
+                ...state,
+                loading : true
+            }
+        case LOADING_FINISH:
+            return {
+                ...state,
+                loading : false
+            }
         default:
             return state
     }
