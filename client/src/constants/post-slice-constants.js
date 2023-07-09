45904725f37feae28ef7eaf08d1991a795fef537
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-10 03:04:08 +0400

    post loading settings

---
+++ client/src/constants/post-slice-constants.js
@@ -0,0 +1,2 @@
+export const LOADING_START = "post-loading-start"
+export const LOADING_FINISH = "post-loading-finish"
--- client/src/redux/reducers/postSlice/postReducer.js
+++ client/src/redux/reducers/postSlice/postReducer.js
@@ -1,5 +1,17 @@
+import { LOADING_FINISH, LOADING_START } from "../../../constants/post-slice-constants"
+
 function postReducer(state={ loading : false, post : {} },action){
     switch(action.type){
+        case LOADING_START:
+            return {
+                ...state,
+                loading : true
+            }
+        case LOADING_FINISH:
+            return {
+                ...state,
+                loading : false
+            }
         default:
             return state
     }
