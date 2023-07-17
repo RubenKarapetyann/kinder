7e026c4eb7d65da5cf543000f046c25d0021b25d
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-17 18:26:35 +0400

    add client add-friends-slice new constants and status types

--- client/src/constants/add-friend-slice-constants.js
+++ client/src/constants/add-friend-slice-constants.js
@@ -1,2 +1,10 @@
 export const LOADING_START = "add-friend-loading-start"
 export const LOADING_FINISH = "add-friend-loading-finish"
+export const LIST_SET = "set-add-friend-list"
+export const SET_STATUS = "set-status"
+
+
+export const FRIENDS = "friends"
+export const NOT_FRIENDS = "not-friends"
+export const YOU_SEND = "you-send"
+export const OTHER_SEND = "other-send"
