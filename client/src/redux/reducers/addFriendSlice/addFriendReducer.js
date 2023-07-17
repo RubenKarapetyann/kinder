9fd7e8e28125770d2b28d0e4d9c2725d1b2796bd
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-17 21:45:51 +0400

    add active friend function

--- client/src/redux/reducers/addFriendSlice/addFriendReducer.js
+++ client/src/redux/reducers/addFriendSlice/addFriendReducer.js
@@ -1,4 +1,8 @@
 import { LOADING_START, LOADING_FINISH } from "../../../constants/add-friend-slice-constants"
+import { getHeaders } from "../../../constants/api-constants"
+import { LOGIN } from "../../../constants/routes-constants"
+import { loadingFinish, loadingStart } from "../../../utils/api-helper"
+import { setStatus } from "./addFriendActions"
 
 
 function addFriendReducer(state={ loading : false, list : [] },action){
@@ -17,5 +21,34 @@ function addFriendReducer(state={ loading : false, list : [] },action){
             return state
     }
 }
+
+
+export const activeFriend = (id,status,navigate)=>{
+    return (dispatch)=>{
+        const token = localStorage.getItem("jwtToken")
+        if(token){
+            try{
+                loadingStart(LOADING_START)
+                fetch("/addfriend",{
+                    method : "POST",
+                    headers : getHeaders(token),
+                    body : JSON.stringify({
+                        id,
+                        status
+                    })
+                }).then(response=>response.json()).then(res=>{
+                    dispatch(setStatus(res.status,res.id))
+                    loadingFinish(LOADING_FINISH)
+                })
+            }catch(err){
+                loadingFinish(LOADING_FINISH)
+                console.log(err);
+            }
+        }else{
+            navigate("/"+LOGIN)
+        }
+    }
+}
+
 
 export default addFriendReducer
