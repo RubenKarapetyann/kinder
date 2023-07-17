b80c916fbf1214ad604c4b16423990b9adb381f6
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-18 00:14:13 +0400

    get friends requests if search is empty

--- client/src/components/header/components/HeaderSearch/HeaderSearch.js
+++ client/src/components/header/components/HeaderSearch/HeaderSearch.js
@@ -1,12 +1,34 @@
-import { useLocation } from "react-router"
+import { useLocation, useNavigate } from "react-router"
 import { NEED_SEARCH } from "../../../../constants/header-constants"
-
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import { getList } from "../../../../utils/api-helper"
+import { LIST_SET, LOADING_START, LOADING_FINISH } from "../../../../constants/add-friend-slice-constants"
+import { ADD_FRIEND } from "../../../../constants/routes-constants"
 
 const HeaderSearch = ()=>{
     const location = useLocation()
     const activeSearch = NEED_SEARCH.find(item=>"/"+item===location.pathname)
+    const [search,setSearch] = useState("")
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        if(location.pathname.slice(1,) === ADD_FRIEND){
+            const id = setTimeout(()=>{
+                dispatch(getList(navigate,location.pathname.slice(1,),"?search="+search,LIST_SET,LOADING_START,LOADING_FINISH))
+            },1000)
+            return ()=>{
+                clearTimeout(id)
+            }
+        }
+    },[search])
+
 
     if(!activeSearch)return null
+
+    const searchHandle = e =>setSearch(e.target.value)
+
 
     return(
         <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
@@ -15,6 +37,8 @@ const HeaderSearch = ()=>{
                 className="form-control" 
                 placeholder="Search..." 
                 aria-label="Search"
+                value={search}
+                onChange={searchHandle}
             />
         </form>
     )
