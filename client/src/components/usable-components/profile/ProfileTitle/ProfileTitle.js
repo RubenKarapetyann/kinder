41e3950d3739e9486af7058abb43ae8767dbf08c
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-17 18:53:11 +0400

    add middleware system for profile title component

--- client/src/components/usable-components/profile/ProfileTitle/ProfileTitle.js
+++ client/src/components/usable-components/profile/ProfileTitle/ProfileTitle.js
@@ -1,13 +1,24 @@
 import "./ProfileTitle.css"
 
-const ProfileTitle = ({ userName, comment, children, avatarImg="https://ionicframework.com/docs/img/demos/avatar.svg" })=>{
+const ProfileTitle = ({ userName, comment, children, avatarImg="https://ionicframework.com/docs/img/demos/avatar.svg", middleware=(children)=>children })=>{
     return(
         <div className="profile-title-container">
-            <img src={avatarImg} alt="mdo" width="32" height="32" className="rounded-circle"/>
+            {middleware(
+                <img 
+                    src={avatarImg} 
+                    alt="mdo" width="32" 
+                    height="32" 
+                    className="rounded-circle"
+                />
+            )}
             <div className="profile-title-info">
                 <div className="profile-title-info-container">
-                    <span className="profile-title-username">{userName}</span>
-                    <span className="profile-title-comment">{comment}</span>
+                    {middleware(
+                        <>
+                            <span className="profile-title-username">{userName}</span>
+                            <span className="profile-title-comment">{comment}</span>
+                        </>
+                    )}
                 </div>
                 <div className="profile-title-function">{children}</div>
             </div>
