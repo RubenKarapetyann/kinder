5b0f70b6936344ccd856ffabcc57ede4e0979a84
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-17 21:31:28 +0400

    finish AddFriend Page

--- client/src/components/pages/add-friend/AddFriend.js
+++ client/src/components/pages/add-friend/AddFriend.js
@@ -1,4 +1,3 @@
-import { useEffect } from "react"
 import Friend from "./components/Friend"
 import { useSelector } from "react-redux"
 import SearchDecoration from "./components/SearchDecoration"
@@ -7,12 +6,7 @@ import "./css/AddFriend.css"
 function AddFriend(){
     const { list, loading } = useSelector(store=>store.addfriend)
 
-    useEffect(()=>{
  
-    },[])
-
- 
-
     if(loading){return<p>loading...</p>}
     return (
         <>
@@ -20,6 +14,10 @@ function AddFriend(){
             {list.map(user=>{
                 return <Friend
                     userName={user.userName}
+                    avatarImg={user.avatarImg}
+                    key={user.id}
+                    id={user.id}
+                    status={user.status}
                 />
             })}
         </>
