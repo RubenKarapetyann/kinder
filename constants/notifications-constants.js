2dbc88ae3ffa365bf7dcb7a2643092663a360150
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-17 18:00:43 +0400

    add add-friend route constant and friend requests notification types

--- constants/notifications-constants.js
+++ constants/notifications-constants.js
@@ -1,4 +1,6 @@
 export const NOTIFICATIONS_TYPES = {
     "like" : "liked you photo",
-    "comment" : "the user left a comment under your post"
+    "comment" : "the user left a comment under your post",
+    "friend-requests" : "user send friend requests",
+    "friend-requests-access" : "user accepted your friend request"
 }
