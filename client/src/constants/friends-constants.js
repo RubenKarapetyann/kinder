fcfb24b12900418082f40b5764c26bb184430028
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-17 18:31:37 +0400

    add GET_USER_STATUS function with icons

--- client/src/constants/friends-constants.js
+++ client/src/constants/friends-constants.js
@@ -1,5 +1,6 @@
-import { FaPaperPlane, FaTrash, FaUser } from "react-icons/fa"
+import { FaPaperPlane, FaTrash, FaUser, FaUserCheck, FaUserPlus, FaUserTimes, FaUsers } from "react-icons/fa"
 import { MESSAGES } from "./routes-constants"
+import { FRIENDS, NOT_FRIENDS, OTHER_SEND, YOU_SEND } from "./add-friend-slice-constants"
 
 export const FRIENDS_DROPDOWN = (id,chatId,handle,navigate,dispatch)=>([
     {
@@ -25,3 +26,12 @@ export const FRIENDS_DROPDOWN = (id,chatId,handle,navigate,dispatch)=>([
         icon : <FaTrash/>
     },
 ])
+
+export const GET_USER_STATUS = (handle,id,dispatch)=>{
+    return {
+        [FRIENDS] : <span className="big-icon"><FaUsers/></span>,
+        [NOT_FRIENDS] : <span onClick={()=>dispatch(handle(id,NOT_FRIENDS))} className="big-icon"><FaUserPlus/></span>,
+        [OTHER_SEND] : <span onClick={()=>dispatch(handle(id,OTHER_SEND))} className="big-icon"><FaUserCheck/></span>,
+        [YOU_SEND] : <span onClick={()=>dispatch(handle(id,YOU_SEND))} className="big-icon"><FaUserTimes/></span>
+    }
+}
