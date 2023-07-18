a66ed8c05a7546f88b44e1321358ef2130c5ef0f
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-19 00:33:44 +0400

    create changeSettings function, that send changes to server

--- client/src/redux/reducers/settingsSlice/settingsReducer.js
+++ client/src/redux/reducers/settingsSlice/settingsReducer.js
@@ -1,4 +1,7 @@
+import { LOGIN } from "../../../constants/routes-constants"
 import { LOADING_FINISH, LOADING_START } from "../../../constants/settings-slice-constants"
+import { loadingFinish, loadingStart } from "../../../utils/api-helper"
+import { setUser } from "../userSlice/UserActions"
 
 function settingsReducer(state={ loading : false },action){
     switch (action.type){
@@ -16,5 +19,35 @@ function settingsReducer(state={ loading : false },action){
             return state
     }
 }
+
+export const changeSettings = (navigate,formData)=>{
+    return (dispatch)=>{
+        const token = localStorage.getItem("jwtToken")
+        if(token){
+            try{
+                dispatch(loadingStart(LOADING_START))
+                fetch("/settings",{
+                    method : "POST",
+                    headers : {
+                        "authorization" : "Bearer "+token
+                    },
+                    body : formData
+                }).then(res=>res.json()).then(result=>{
+                    if(result.access){
+                        dispatch(setUser(result.user))
+                        navigate(-1)
+                    }
+                    dispatch(loadingFinish(LOADING_FINISH))
+                })
+            }catch(err){
+                dispatch(loadingFinish(LOADING_FINISH))
+                navigate(-1)
+            }
+        }else{
+            navigate("/"+LOGIN)
+        }
+    }
+}
+
 
 export default settingsReducer
