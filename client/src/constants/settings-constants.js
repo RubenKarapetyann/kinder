78abe44470807f82484600f5a6afa43bfff9bede
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-19 00:43:20 +0400

    add settings routes array

--- client/src/constants/settings-constants.js
+++ client/src/constants/settings-constants.js
@@ -1,5 +1,6 @@
 import { FaSignOutAlt, FaUser } from "react-icons/fa"
 import { LOG_OUT } from "./routes-constants"
+import Window from "../components/pages/settings/components/Window"
 
 export const SETTINGS_ARR = (avatar)=>[
     {
@@ -38,5 +39,30 @@ export const SETTINGS_ARR = (avatar)=>[
         title : "Sign Out",
         routeName : "/"+LOG_OUT,
         icon : <FaSignOutAlt/>
+    }
+]
+
+
+export const SETTINGS_ROUTES = ()=>[
+    {
+        title : "username",
+        id : "username",
+        routeName : "username",
+        element : <Window title={"name"} type={"text"}/>,
+        takeFromUser : "name"
+    },
+    {
+        title : "description",
+        id : "description",
+        routeName : "description",
+        element : <Window title={"description"} type={"text"}/>,
+        takeFromUser : "description"
+    },
+    {
+        title : "avatar",
+        id : "avatar",
+        routeName : "avatar",
+        element : <Window title={"avatarImg"} type={"file"}/>,
+        takeFromUser : "avatarImg"
     }
 ]
