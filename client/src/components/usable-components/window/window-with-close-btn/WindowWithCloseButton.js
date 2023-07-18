bb54c9f1347a3d0766af191100236f910580d42a
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-18 23:11:20 +0400

    add window-with-close-button usable component

---
+++ client/src/components/usable-components/window/window-with-close-btn/WindowWithCloseButton.js
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router"
+
+const WindowWithCloseButton = ({ children, title }) =>{
+    const navigate = useNavigate()
+    const closeHandle = ()=>navigate(-1)
+    return (
+        <div className="window-background">
+            <div className="modal-content rounded-4 shadow window-container">
+                <div className="modal-header border-bottom-0">
+                    <h1 className="modal-title fs-5">{title}</h1>
+                    <button onClick={closeHandle} type="button" className="btn-close" aria-label="Close"></button>
+                </div>
+                <br/>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default WindowWithCloseButton
