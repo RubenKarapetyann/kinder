d62ae914715b1eb65cde29e0c6756ecf53d577cb
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-22 01:29:28 +0400

    ProfilePostsList gets list by active from state

--- client/src/components/pages/profile/components/ProfilePostsList.js
+++ client/src/components/pages/profile/components/ProfilePostsList.js
@@ -1,11 +1,15 @@
 import { useSelector } from "react-redux"
 import ProfilePost from "./ProfilePost"
+import { PROFILE_TABS_MAP } from "../../../../constants/profile-constants"
 
 const ProfilePostsList = ()=>{
-    const posts = useSelector(store=>store.profile.profile.posts)
+    const activeTab = useSelector(store=>store.profile.activeTab)
+    const list = useSelector(store=>store.profile.profile[PROFILE_TABS_MAP[activeTab].getFromState])
+
+
     return (
         <div className="profile-posts-container">
-            {posts.map(post=>{
+            {list && list.map(post=>{
                 return <ProfilePost img={post.img} key={post.postId} id={post.postId}/>
             })}
         </div>
