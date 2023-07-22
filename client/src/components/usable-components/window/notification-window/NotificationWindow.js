7ad48c04bb81fd08bd15964bceaea2c4ce112c5d
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-22 23:52:37 +0400

    create notification window files

---
+++ client/src/components/usable-components/window/notification-window/NotificationWindow.js
@@ -0,0 +1,9 @@
+import "./NotificationWindow.css"
+
+const NotificationWindow = ({ text })=>{
+    return (
+        <div className="notification-window-container">{text}</div>
+    )
+}
+
+export default NotificationWindow
