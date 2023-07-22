df43c2ab016ee22bece5b084d3390909bdd7b253
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-07-22 22:34:32 +0400

    newPost component new style v1

--- client/src/components/pages/new-post/NewPost.js
+++ client/src/components/pages/new-post/NewPost.js
@@ -1,23 +1,10 @@
-import { useRef, useState } from "react"
 import "./css/NewPost.css"
 import PostDescription from "./components/PostDescription";
 
 function NewPost(){
 
-    const [file,setFile] = useState()
-    
-
     return(
-        <PostDescription file={file}>
-            <label htmlFor="formFile" className="form-label"></label>
-            <input 
-                onChange={(e)=>setFile(e.target.files[0])} 
-                accept=".png, .jpg, .jpeg" 
-                className="form-control" 
-                type="file" 
-                id="formFile" 
-                name="file"
-            />
+        <PostDescription>
             {loading=><button className="btn btn-primary" disabled={loading}>Post</button>}
         </PostDescription>
     )   
--- client/src/components/pages/new-post/components/PostDescription.js
+++ client/src/components/pages/new-post/components/PostDescription.js
@@ -2,22 +2,28 @@ import { useEffect, useRef, useState } from "react"
 import FormInput from "../../../usable-components/form/FormInput"
 import { useNavigate } from "react-router"
 import { HOME } from "../../../../constants/routes-constants"
+import { FaUpload } from "react-icons/fa"
+import images from "../../../../images/more/images.svg"
 
-const PostDescription = ({ children, file })=>{
+const PostDescription = ({ children })=>{
     const [value,setValue] = useState("")
     const changeHandle = e => setValue(e.target.value)
     const formData = useRef(new FormData())
     const navigate = useNavigate()
     const [loading, setLoading] = useState(false)
-
+    const reader = new FileReader()
+    const [file,setFile] = useState(null)
 
     useEffect(()=>{
         if(file){
             formData.current.set("file",file,"post.jpg")
+            reader.readAsDataURL(file)
+            reader.onload = function() {
+                setFile(reader.result)
+            }
         }
         formData.current.set("description",value)
     },[value,file])
-
 
     const submitHandle = e =>{
         e.preventDefault()
@@ -45,11 +51,22 @@ const PostDescription = ({ children, file })=>{
 
     return(
         <form className="posting-container" onSubmit={submitHandle}>
-            <div className="mb-3">
-                {children.slice(0,2)}
+            <div className="dropzone">
+                <img src={images}/>
+                <input 
+                    type="file" 
+                    className="new-post-file-input" 
+                    id="images" 
+                    accept="image/png, image/jpeg, image/jpg" 
+                    multiple=""
+                />
+                <label htmlFor="images" className="new-post-file-label"><FaUpload/> Upload Image</label>
+
+                {/* <h3>or drag &amp; drop your PNG or JPEG files here</h3> */}
             </div>
+            <br/>
             <FormInput name={"Description"} type={"text"} value={value} changeHandle={changeHandle}/> 
-            {children[2](loading)}
+            {children(loading)}
         </form>
     )
 }
