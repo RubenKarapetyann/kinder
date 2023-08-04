23592ee7fbacefca9a16fea4d6d46f37e42b26a2
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-08-04 16:23:30 +0400

    add friends-messages list if window width > 800

--- client/src/components/pages/chat/Chat.js
+++ client/src/components/pages/chat/Chat.js
@@ -2,18 +2,21 @@ import { useDispatch } from "react-redux"
 import { getChatList, sendMessage } from "../../../redux/reducers/chatSlice/chatReducer"
 import InputControl from "../../usable-components/messages/InputControl"
 import MessageBox from "../../usable-components/messages/MessageBox"
-import { useParams } from "react-router"
+import { useNavigate, useParams } from "react-router"
 import { useEffect, useRef } from "react"
 import { addMessage } from "../../../redux/reducers/chatSlice/chatActions"
 import io from "socket.io-client"
 import { SERVER_URL } from "../../../constants/api-constants"
-
+import "./css/Chat.css"
+import Messages from "../messages/Messages"
+import { getMessagesList } from "../../../redux/reducers/messagesSlice/messagesReducer"
 
 function Chat(){
 
     const socketRef = useRef(null)
     const dispatch = useDispatch()
     const { id } = useParams()
+    const navigate = useNavigate()
 
     useEffect(()=>{
         socketRef.current = io(SERVER_URL, {
@@ -22,19 +25,25 @@ function Chat(){
 
         socketRef.current.on("message:add",(message)=>{
             dispatch(addMessage(message))
+            if(window.innerWidth > 800){
+                dispatch(getMessagesList(navigate))
+            }
         })
 
         return () => {
             socketRef.current.disconnect()
         }
-    },[])
+    },[id])
 
 
     return(
-        <>
-            <MessageBox type={"chat"} handle={getChatList}/>
-            <InputControl type={"chat"} handle={sendMessage} socket={socketRef}/>
-        </> 
+        <div className="chat-page-container">
+            <div className="chat-page-messages-list"><Messages/></div>
+            <div className="chat-page-messages-box">
+                <MessageBox type={"chat"} handle={getChatList}/>
+                <InputControl type={"chat"} handle={sendMessage} socket={socketRef}/>
+            </div>
+        </div> 
     )
 }
 
